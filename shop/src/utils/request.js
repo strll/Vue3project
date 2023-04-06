c4cd4712a349c16ad961ab8c48749875726f0245
strnull <1290366985@qq.com>
2023-04-06 22:35:15 +0800

    后台管理app端用户管理

--- shop/src/utils/request.js
+++ shop/src/utils/request.js
@@ -8,17 +8,11 @@ export const loginApi=data=>{
 }
 
 
-export const first_getuserPage=data=>{
-    return get({
 
-        url:`/appuser/first_getuserPage?size=${data}`,
-
-    })
-}
-export const getuserPage=(size,number)=>{
+export const getuserPage=()=>{
     // eslint-disable-next-line no-undef
     return get({
-        url:`/appuser/getuserPage?size=${size}&number=${number}`,
+        url:`/appuser/getuserPage`,
     })
 }
 
